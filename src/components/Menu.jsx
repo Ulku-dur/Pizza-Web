a8e04e721cd5db960a1f0161143ad679b5698c75
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-11-28 22:26:37 +0300

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,39 +1,46 @@
+import img1 from "../assets/img/menu1.jpg";
+import img2 from "../assets/img/menu2.jpg";
+import img3 from "../assets/img/menu3.jpg";
+import img4 from "../assets/img/menu4.jpg";
+import img5 from "../assets/img/menu5.jpg";
+import img6 from "../assets/img/menu6.jpg";
 import MenuCard from "../layouts/MenuCard";
+
 const Menu = () => {
   const menuData = [
     {
       id: 1,
-      img: "src/assets/img/menu1.jpg",
+      img: img1,
       title: "Margarita Marvel",
       price: "16.99",
     },
     {
       id: 2,
-      img: "src/assets/img/menu2.jpg",
+      img: img2,
       title: "Pesto Paradise",
       price: "18.99",
     },
     {
       id: 3,
-      img: "src/assets/img/menu3.jpg",
+      img: img3,
       title: "Hawaiian Bliss",
       price: "14.99",
     },
     {
       id: 4,
-      img: "src/assets/img/menu4.jpg",
+      img: img4,
       title: "Truffle Treasures",
       price: "15.99",
     },
     {
       id: 5,
-      img: "src/assets/img/menu5.jpg",
+      img: img5,
       title: "Farmhouse Harvest",
       price: "18.99",
     },
     {
       id: 6,
-      img: "src/assets/img/menu6.jpg",
+      img: img6,
       title: "Cheese Frenzy",
       price: "13.99",
     },
