8423a722a6b7b09a7d19e2b94d4451acf7aae09c
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-11-28 23:22:17 +0300

    Update OtherMenu.jsx

--- src/components/OtherMenu.jsx
+++ src/components/OtherMenu.jsx
@@ -1,28 +1,32 @@
 import MenuCard from "../layouts/MenuCard";
+import img7 from "../assets/img/menu7.jpg";
+import img8 from "../assets/img/menu8.jpg";
+import img9 from "../assets/img/menu9.jpg";
+import img10 from "../assets/img/menu10.jpg";
 
 const OtherMenu = () => {
   const menuData = [
     {
       id: 7,
-      img: "src/assets/img/menu7.jpg",  // Görsel yolu düzeltildi
+      img: img7,  
       title: "Margarita Marvel",
       price: "16.99",
     },
     {
       id: 8,
-      img: "src/assets/img/menu8.jpg",
+      img: img8,
       title: "Pesto Paradise",
       price: "18.99",
     },
     {
       id: 9,
-      img: "src/assets/img/menu9.jpg",
+      img: img9,
       title: "Hawaiian Bliss",
       price: "14.99",
     },
     {
       id: 10,
-      img: "src/assets/img/menu10.jpg",
+      img: img10,
       title: "Truffle Treasures",
       price: "15.99",
     },
