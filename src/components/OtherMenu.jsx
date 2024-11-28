1f46c72e3c155cce622f72b587ffe74cca11cbb6
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-11-28 23:25:26 +0300

    Update OtherMenu.jsx

--- src/components/OtherMenu.jsx
+++ src/components/OtherMenu.jsx
@@ -1,44 +1,40 @@
+import React from "react";
 import MenuCard from "../layouts/MenuCard";
-import img7 from "../assets/img/menu7.jpg";
-import img8 from "../assets/img/menu8.jpg";
-import img9 from "../assets/img/menu9.jpg";
-import img10 from "../assets/img/menu10.jpg";
 
 const OtherMenu = () => {
   const menuData = [
     {
       id: 7,
-      img: img7,  
+      img: "src/assets/img/menu7.jpg",
       title: "Margarita Marvel",
       price: "16.99",
     },
     {
       id: 8,
-      img: img8,
+      img: "src/assets/img/menu8.jpg",
       title: "Pesto Paradise",
       price: "18.99",
     },
     {
       id: 9,
-      img: img9,
+      img: "src/assets/img/menu9.jpg",
       title: "Hawaiian Bliss",
       price: "14.99",
     },
     {
       id: 10,
-      img: img10,
+      img: "src/assets/img/menu10.jpg",
       title: "Truffle Treasures",
       price: "15.99",
     },
   ];
-
   return (
-    <div className="min-h-screen container flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-semibold text-center mt-20 md:mt-10 mb-3">
+    <div className="min-h-screen container flex flex-col justify-center items-center ">
+      <h1 className=" text-4xl font-semibold text-center mt-20 md:mt-10 mb-3">
         Other Menu
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4 gap-5 px-0 md:px-10">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4 gap-5 px-0 md:px-10">
         {menuData.map((item) => (
           <MenuCard
             key={item.id}
